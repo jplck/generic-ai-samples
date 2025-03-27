4dc87f094d81bcd2577877b8355cc3eed96cf44b
jplck <pollack.jan@gmail.com>
2025-03-27 08:43:09 +0000

    Refactor Azure model initialization in llm.py for improved flexibility and clarity

--- samples/chat_langgraph/llm.py
+++ samples/chat_langgraph/llm.py
@@ -2,28 +2,31 @@ import os
 from typing import List
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
-token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
-api_key = os.getenv("AZURE_OPENAI_API_KEY")
+from langchain.chat_models import init_chat_model
 
-def prepare_azure_openai_completion_model(callbacks: List[callable]) -> AzureChatOpenAI:
-    return AzureChatOpenAI(
-        azure_ad_token_provider= token_provider if not api_key else None,
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key= api_key if api_key else None,
-        azure_deployment=os.getenv("AZURE_OPENAI_COMPLETION_DEPLOYMENT_NAME"),
-        openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-        temperature=0,
-        streaming=True,
-        model_kwargs={"stream_options":{"include_usage": True}},
-        callbacks=callbacks,
-        openai_api_type= "api_key" if api_key else "azure_ad",
-    )
+def get_model_on_azure(deployment_name: str, temperature: float = 0.0, callbacks: List = None):
+    
+    #If you stick to the default environment variables, you can remove the model_kwargs.
+    
+    model_kwargs={
+        "api_version": os.getenv("AZURE_OPENAI_API_VERSION"),
+        "azure_endpoint": os.getenv("AZURE_OPENAI_ENDPOINT"),
+        "callbacks": callbacks,
+    }
 
-def prepare_azure_openai_embeddings_model() -> AzureOpenAIEmbeddings:
-    return AzureOpenAIEmbeddings(    
-        azure_deployment = os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME"),
-        openai_api_version = os.getenv("AZURE_OPENAI_API_VERSION"),
-        model= os.getenv("AZURE_OPENAI_EMBEDDING_MODEL"),
-        azure_ad_token_provider = token_provider if not api_key else None,
-        api_key=api_key if api_key else None
-    )
+    api_key = os.getenv("AZURE_OPENAI_API_KEY")
+    if api_key:
+        model_kwargs["api_key"] = api_key
+    else:
+        model_kwargs["azure_ad_token_provider"] = get_bearer_token_provider(
+            DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+        )
+
+    return init_chat_model(deployment_name, model_provider="azure_openai", temperature=temperature, model_kwargs=model_kwargs)
+
+def get_github_model():
+    model_kwargs = {
+        "api_key": os.getenv("GITHUB_MODELS_TOKEN"),
+        "base_url": "https://models.inference.ai.azure.com",
+    }
+    return init_chat_model(model="gpt-4o", model_provider="openai", temperature=0.0, model_kwargs=model_kwargs)
